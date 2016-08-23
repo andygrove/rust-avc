e7f483a4bed6b4d4f2aebaa9e9250fb6185b31fe
Andy Grove <andygrove73@gmail.com>
2016-08-23 06:40:15 -0600

    code cleanup

--- src/gps.rs
+++ src/gps.rs
@@ -31,32 +31,41 @@ impl GPS {
         let f = self.filename.clone();
         let gps_location = self.location.clone();
 
+        let mut port = serial::open(f).unwrap();
+
+        port.reconfigure(&|settings| {
+            settings.set_baud_rate(serial::Baud57600).unwrap();
+            settings.set_char_size(serial::Bits8);
+            settings.set_parity(serial::ParityNone);
+            settings.set_stop_bits(serial::Stop1);
+            settings.set_flow_control(serial::FlowNone);
+            Ok(())
+        }).unwrap();
+
+        port.set_timeout(Duration::from_millis(5000)).unwrap();
+
         // start thread to read from serial port
         let handle = thread::spawn(move || {
-            let mut port = serial::open(f).unwrap();
 
-            port.reconfigure(&|settings| {
-                settings.set_baud_rate(serial::Baud57600).unwrap();
-                settings.set_char_size(serial::Bits8);
-                settings.set_parity(serial::ParityNone);
-                settings.set_stop_bits(serial::Stop1);
-                settings.set_flow_control(serial::FlowNone);
-                Ok(())
-            }).unwrap();
-
-            port.set_timeout(Duration::from_millis(5000)).unwrap();
+            let mut buf = vec![0_u8; 128];
+            let mut read_buf = vec![0_u8; 128];
 
             loop {
-                println!("Reading...");
-                let mut buf = vec![0_u8; 50];
-                let bytes_read = port.read(&mut buf[..]).unwrap();
+                println!("Reading from GPS...");
+                let n = port.read(&mut read_buf[..]).unwrap();
+
+                println!("Read {} bytes from GPS...", n);
+                buf.extend_from_slice(&read_buf[0..n]);
+
+                //TODO: parse NMEA sentence
+
+                //TODO: only update location if GPS has changed
 
                 // on receive valid co-ords ...
                 let mut loc = gps_location.lock().unwrap();
                 loc.set(12.3 as f64, 45.6 as f64);
 
             }
-
         });
     }
 
