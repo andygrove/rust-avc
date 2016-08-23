5609bc6aef59747c51951fd10647fef7f62cc6c3
Andy Grove <andygrove73@gmail.com>
2016-08-22 20:06:16 -0600

    save

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,35 @@ impl GPS {
     }
 }
 
+struct Compass {
+    filename: &'static str
+}
+
+impl Compass {
+
+    fn new(f: &'static str) -> Self {
+        Compass { filename: f }
+    }
+
+    fn get(&self) -> f32 {
+        //TODO: get real compass bearing
+        349.5
+    }
+
+}
+
+struct Motor {
+    filename: &'static str
+}
+
 fn main() {
     println!("Hello, world!");
 
     let gps = GPS::new("/dev/ttyUSB0");
+    let compass = Compass::new("/dev/ttyUSB1");
 
     let test = gps.get();
+
 
     println!("Location: {:?}", test);
 }
