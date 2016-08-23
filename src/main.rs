1c4ccfcb9aaa37ef143ffba34d0fb2148729ac3b
Andy Grove <andygrove73@gmail.com>
2016-08-22 20:18:45 -0600

    save

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use navigation::*;
 
 mod gps;
 mod compass;
+mod motors;
 
 use gps::GPS;
 use compass::Compass;
+use motors::Motors;
+
+
+//enum Action {
+//
+//}
 
 
 fn main() {
@@ -14,8 +21,27 @@ fn main() {
 
     let gps = GPS::new("/dev/ttyUSB0");
     let compass = Compass::new("/dev/ttyUSB1");
+    let motors = Motors::new("/dev/ttyUSB2");
 
-    let test = gps.get().unwrap();
+    //TODO: wait for start button
 
-    println!("Location: {:?}", test);
+    //TODO: load waypoints from file
+    let waypoints: Vec<Location> = vec![
+        Location::new(39.8617, -104.6731),
+        Location::new(39.8617, -104.6731),
+        Location::new(39.8617, -104.6731),
+    ];
+
+    for (i, waypoint) in waypoints.iter().enumerate() {
+        println!("Heading for waypoint {} at {:?}", i+1, waypoint);
+
+        let test = gps.get().unwrap();
+
+        println!("Location: {:?}", test);
+
+
+    }
+
+    println!("Finished");
+
 }
