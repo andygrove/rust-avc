b56b5a39533548080d1c38995cca3e0c9be0aa48
Andy Grove <andygrove73@gmail.com>
2016-08-22 20:03:24 -0600

    rough out GPS struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate navigation;
+
+use navigation::*;
+
+
+struct GPS {
+    filename: &'static str
+}
+
+impl GPS {
+
+    fn new(f: &'static str) -> Self {
+        GPS { filename: f }
+    }
+
+    fn get(&self) -> Location {
+        //TODO: get real location
+        Location::new(39.8617, -104.6731)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let gps = GPS::new("/dev/ttyUSB0");
+
+    let test = gps.get();
+
+    println!("Location: {:?}", test);
 }
