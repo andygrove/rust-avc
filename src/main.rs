5a9c4e76bc47941c088f27dc62a4123833e2f8ab
Andy Grove <andygrove73@gmail.com>
2016-08-27 11:24:57 -0600

    impl test avc mode

--- src/main.rs
+++ src/main.rs
@@ -70,15 +70,24 @@ fn calc_bearing_diff(current_bearing: f64, wp_bearing: f64) -> f64 {
     ret
 }
 
-fn navigate_to_waypoint(car: &mut Car, wp_num: usize, wp: &Location) {
+fn navigate_to_waypoint(video: &Video, enable_motors: bool, car: &mut Car, wp_num: usize, wp: &Location) {
     car.set_action(Action::Navigating { waypoint: wp_num });
     loop {
+
+        video.capture();
+
+        video.draw_text(30, 30, format!("UTC: {}", UTC::now()));
+
         match car.gps.get() {
             None => {
+                video.draw_text(30, 30, format!("GPS: N/A"));
                 car.set_action(Action::WaitingForGps);
-                car.motors.coast();
+                if enable_motors {
+                    car.motors.coast();
+                }
             },
             Some(loc) => {
+                video.draw_text(30, 30, format!("GPS: {}, {}", loc.lat, loc.lon));
                 if close_enough(&loc, &wp) {
                     car.set_action(Action::ReachedWaypoint { waypoint: wp_num });
                     break;
@@ -86,28 +95,36 @@ fn navigate_to_waypoint(car: &mut Car, wp_num: usize, wp: &Location) {
 
                 match car.compass.get() {
                     None => {
+                        video.draw_text(30, 50, format!("Compass: N/A"));
                         car.set_action(Action::WaitingForCompass);
-                        car.motors.coast();
+                        if enable_motors {
+                            car.motors.coast();
+                        }
                     },
                     Some(b) => {
+                        video.draw_text(30, 50, format!("Compass: {}", b));
                         let wp_bearing = loc.calc_bearing_to(&wp);
 
                         let turn_amount = calc_bearing_diff(b, wp_bearing);
 
-                        if turn_amount < 0_f64 {
-                            car.motors.set_speed(100, 200);
-                        } else {
-                            car.motors.set_speed(200, 100);
+                        if enable_motors {
+                            if turn_amount < 0_f64 {
+                                car.motors.set_speed(100, 200);
+                            } else {
+                                car.motors.set_speed(200, 100);
+                            }
                         }
                     }
                 }
             }
         };
-        thread::sleep(Duration::from_millis(100));
+
+        video.draw_text(30, 100, format!("{:?}", car.action));
+        video.write();
     }
 }
 
-fn avc() {
+fn avc(enable_motors: bool) {
 
     //TODO: load waypoints from file
     let waypoints: Vec<Location> = vec![
@@ -136,11 +153,14 @@ fn avc() {
     // navigate to each waypoint in turn
     for (i, waypoint) in waypoints.iter().enumerate() {
         println!("Heading for waypoint {} at {:?}", i+1, waypoint);
-        navigate_to_waypoint(&mut car, i+1, &waypoint);
+        navigate_to_waypoint(&video, enable_motors, &mut car, i+1, &waypoint);
     }
 
     car.set_action(Action::Finished);
-    car.motors.stop();
+
+    if enable_motors {
+        car.motors.stop();
+    }
 
     video.close();
 
@@ -221,6 +241,8 @@ fn main() {
     opts.optflag("c", "test-compass", "tests the compass");
     opts.optflag("m", "test-motors", "tests the motors");
     opts.optflag("u", "test-ultrasonic", "tests the ultrasonic sensors");
+    opts.optflag("t", "test-avc", "tests the full avc logic, but without motors running");
+    opts.optflag("a", "avc", "navigate a course, for real!");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -232,8 +254,8 @@ fn main() {
     else if matches.opt_present("c") { test_compass(); }
     else if matches.opt_present("m") { test_motors(); }
     else if matches.opt_present("u") { panic!("not implemented"); }
-    else {
-        avc();
-    }
+    else if matches.opt_present("t") { avc(false); }
+    else if matches.opt_present("a") { avc(true); }
+    else { panic!("missing cmd line argument .. try --help"); }
 
 }
